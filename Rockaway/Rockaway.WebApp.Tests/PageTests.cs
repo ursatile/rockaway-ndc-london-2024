bb970dfff8a8dbcd88be92edf840c0c557b955d8
Dylan Beattie <dylan@dylanbeattie.net>
2024-01-29 10:10:42 +0000

    Examples of using Shouldly and AngleSharp to inspect page markup

--- Rockaway/Rockaway.WebApp.Tests/PageTests.cs
+++ Rockaway/Rockaway.WebApp.Tests/PageTests.cs
@@ -1,4 +1,6 @@
+using AngleSharp;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Shouldly;
 
 namespace Rockaway.WebApp.Tests;
 
@@ -15,10 +17,24 @@ public class PageTests {
 	[InlineData("/")]
 	[InlineData("/privacy")]
 	[InlineData("/contact")]
+	[InlineData("/about")]
 	public async Task Page_Returns_Success(string path) {
 		await using var factory = new WebApplicationFactory<Program>();
 		using var client = factory.CreateClient();
 		using var response = await client.GetAsync(path);
 		response.EnsureSuccessStatusCode();
+	}
+
+	[Theory]
+	[InlineData("/", "Home Page")]
+	public async Task Page_Has_Correct_Title(string path, string title) {
+		var browsingContext = BrowsingContext.New(Configuration.Default);
+		await using var factory = new WebApplicationFactory<Program>();
+		using var client = factory.CreateClient();
+		var html = await client.GetStringAsync(path);
+		var dom = await browsingContext.OpenAsync(req => req.Content(html));
+		var titleTag = dom.QuerySelector("title");
+		titleTag.ShouldNotBeNull();
+		titleTag.InnerHtml.ShouldBe(title);
 	}
 }
