781acf217278ff029c769206bb0c10600f600ee0
Dylan Beattie <dylan@dylanbeattie.net>
2024-01-30 11:57:43 +0000

    Example of fancy panel tag helper

---
+++ Rockaway/Rockaway.WebApp/TagHelpers/FancyPanelTagHelper.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Rockaway.WebApp.TagHelpers;
+
+public class FancyPanelTagHelper : TagHelper {
+	public override void Process(TagHelperContext context, TagHelperOutput output) {
+		base.Process(context, output);
+		output.TagMode = TagMode.StartTagAndEndTag;
+		output.TagName = "div";
+		output.Attributes.Add("class", "fancy-panel");		
+	}
+}
